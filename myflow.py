4fe29d0809a78a8221e2779721a5b52dd8c42874
agrawalshrawan245 <35890883+agrawalshrawan245@users.noreply.github.com>
2020-06-20 03:04:49 +0530

    Update myflow.py

--- myflow.py
+++ myflow.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pdb
+
 
 class Operation():
 
@@ -16,6 +16,9 @@ class Variable():
         self.output = initial_value
         self.output_nodes = []
 
+    def predict(self, feed_dict):
+        pass
+
     def fit(self, del_val):
         self.output = self.output - del_val
 
@@ -23,6 +26,10 @@ class Placeholder():
 
     def __init__(self):
         self.output_nodes = []
+
+    def predict(self, feed_dict):
+        self.output = feed_dict[self]
+        return self.output
 
     def fit(self, del_val):
         pass
@@ -32,9 +39,13 @@ class multiply(Operation):
     def __init__(self, input_nodes= []):
         super().__init__(input_nodes = input_nodes)
 
-    def predict(self, x_var, y_var):
-        
-        return np.multiply(x_var, y_var)
+    def predict(self, feed_dict):
+        for input_node in self.input_nodes:
+            input_node.predict(feed_dict)
+        inputs = [input_node.output for input_node in self.input_nodes]
+
+        self.output = np.multiply(*inputs)
+        return self.output
 
     def fit(self, del_val):
         for input_node in self.input_nodes:
@@ -45,12 +56,17 @@ class add(Operation):
     def __init__(self, input_nodes= []):
         super().__init__(input_nodes = input_nodes)
 
-    def predict(self, x, y):
-        return np.add(x, y)
+    def predict(self, feed_dict):
+        for input_node in self.input_nodes:
+            input_node.predict(feed_dict)
+        inputs = [input_node.output for input_node in self.input_nodes]
+
+        self.output = np.add(*inputs)
+        return self.output
 
     def fit(self, del_val):
         for input_node in self.input_nodes:
-            input_node.fit(del_val * self.output / input_node.output)
+            input_node.fit(del_val)
 
 
 class reduced_mean(Operation):
@@ -59,13 +75,16 @@ class reduced_mean(Operation):
         super().__init__(input_nodes = input_nodes)
 
     def predict(self, feed_dict):
-        a = input_nodes[0]
-        b = input_nodes[1]
-        return np.square(a.output - b.output)
+        for input_node in self.input_nodes:
+            input_node.predict(feed_dict)
+        inputs = [input_node.output for input_node in self.input_nodes]
+
+        self.output = np.square(inputs[0] - inputs[1])
+        return self.output
 
     def fit(self, del_val):
-        a = input_nodes[0]
-        b = input_nodes[1]
+        a = self.input_nodes[0]
+        b = self.input_nodes[1]
         delta = 2 * (a.output - b.output)
         a.fit(del_val * delta)
         b.fit(-del_val * delta)
@@ -78,19 +97,7 @@ class Session():
         self.variable_lis = []
 
     def predict(self, node, feed_dict = {}):
-
-        if type(node) == Placeholder:
-            node.output = feed_dict[node]
-
-        elif type(node) == Variable:
-            pass
-        else:
-            for input_node in node.input_nodes:
-                self.predict(input_node, feed_dict)
-            
-            inputs = [input_node.output for input_node in node.input_nodes]
-            node.output = node.predict(*inputs)
-
+        node.predict(feed_dict)
 
     def fit(self, node, l_rate, feed_dict = {}):
         self.predict(node, feed_dict)
@@ -98,7 +105,7 @@ class Session():
 
 
 
-A = Variable(np.array([5]))
+A = Variable(np.array([3]))
 b = Variable(np.array([2]))
 x = Placeholder()
 y = multiply([A,x])
@@ -107,23 +114,13 @@ y_true = Placeholder()
 loss = reduced_mean([y_true, z])
 
 sess = Session()
-for _ in range(30):
+for _ in range(500):
     temp = np.random.rand()
-    sess.fit(node = z, l_rate = np.array([1]), feed_dict={x:[2*temp], y_true:[temp + 3]})
+    sess.fit(node = loss, l_rate = np.array([0.4]), feed_dict={x:[temp], y_true:[2 * temp + 8]})
 
-print(A.output)
-print(b.output)
+print("Earlier: [3.] * " + str(x.output) + " + [2.] = " + str(np.add(np.multiply(3, x.output), 2)))
+print("Now:     " + str(A.output) + " * " + str(x.output) + " + " + str(b.output) + " = " + str(y.output))
 
-
-# A = Variable(np.array([6]))
-# x = Placeholder()
-# y = multiply([A,x])
-
-# predict(y, feed_dict = {x:np.array([3])})
-
-# fit(y, np.array([1]), feed_dict = {x:np.array([3])})
-
-# print(A.output)
 
 
 
